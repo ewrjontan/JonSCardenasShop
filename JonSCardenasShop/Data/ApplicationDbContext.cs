b8cf670c41506ca0d232f309956240d1ace468f6
Jonathan Cardenas <jonscardenas@gmail.com>
2022-02-23 16:09:00 -0600

    setup db, added search function

--- JonSCardenasShop/Data/ApplicationDbContext.cs
+++ JonSCardenasShop/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using JonSCardenasShop.Models;
 
 namespace JonSCardenasShop.Data
 {
@@ -12,5 +13,6 @@ namespace JonSCardenasShop.Data
             : base(options)
         {
         }
+        public DbSet<JonSCardenasShop.Models.Product> Product { get; set; }
     }
 }
---
+++ JonSCardenasShop/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JonSCardenasShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Price { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
